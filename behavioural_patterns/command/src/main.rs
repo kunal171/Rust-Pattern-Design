ce86110744b9ba89d7ecedc3f81b8c753b3c0656
kunal171 <kunalkunal63@gmail.com>
2024-02-06 18:00:15 +0530

    command Design Patterns

--- behavioural_patterns/command/src/main.rs
+++ behavioural_patterns/command/src/main.rs
@@ -30,6 +30,7 @@ struct Schema {
     commands: Vec<Box<dyn Migration>>,
 }
 
+
 impl Schema {
     fn new() -> Self {
         Self { commands: vec![]}
@@ -38,4 +39,28 @@ impl Schema {
     fn add_migration (&mut self, cmd:Box<dyn Migration>) {
         self.commands.push(cmd);
     }
+
+    fn execute(&self) ->Vec<&str> {
+        self.commands.iter().map(|cmd| cmd.execute()).collect()
+    }
+
+    fn rollback(&self) -> Vec<&str> {
+        self.commands
+            .iter()
+            .rev() // reverse iterator's direction
+            .map(|cmd| cmd.rollback())
+            .collect()
+    }
+}
+
+fn main() {
+    let mut schema = Schema::new();
+
+    let cmd = Box::new(CreateTable);
+    schema.add_migration(cmd);
+    let cmd = Box::new(AddField);
+    schema.add_migration(cmd);
+
+    assert_eq!(vec!["create table", "add field"], schema.execute());
+    assert_eq!(vec!["remove field", "drop table"], schema.rollback());
 }
